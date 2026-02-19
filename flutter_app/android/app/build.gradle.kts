24ad6d55c5941014a249ce6435dcfb9006581f7f
Karl <karlchristian8000@gmail.com>
2026-02-19 16:08:33 +0800

    keystore fix

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -9,6 +9,16 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    // Added this block to point to your root keystore
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("../../debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -28,7 +38,11 @@ android {
     }
 
     buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
+            // Updated this to use the debug signing config for collaboration
             signingConfig = signingConfigs.getByName("debug")
         }
     }
